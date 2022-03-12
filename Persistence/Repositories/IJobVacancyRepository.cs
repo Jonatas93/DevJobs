7a812cf24bbfc6eef054611f99c5d3b556cc243b
Jonatas Silva <jonatasbatistasilva@gmail.com>
2022-03-11 23:04:02 -0300

    atualizaçao de classe

--- Persistence/Repositories/IJobVacancyRepository.cs
+++ Persistence/Repositories/IJobVacancyRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace DevJobs.API.Persistence.Repositories
 {
-    public interface IJobVacancyRepository
+    public class IJobVacancyRepository
     {
         
     }
